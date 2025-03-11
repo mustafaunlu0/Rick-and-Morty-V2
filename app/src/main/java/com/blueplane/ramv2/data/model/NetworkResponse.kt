87f0f3ae0c9905d31017b9de43cc4f702d0eab43
Mustafa Ünlü <mustafa.unlu@erlab.com>
2025-03-11 11:58:21 +0300

    Removed the blanks.

--- app/src/main/java/com/blueplane/ramv2/data/model/NetworkResponse.kt
+++ app/src/main/java/com/blueplane/ramv2/data/model/NetworkResponse.kt
@@ -6,8 +6,6 @@ sealed class NetworkResponse <out T : Any?>{
     data class Error(val error : Exception) : NetworkResponse<Nothing>()
     data class Success<out T : Any>(val data : T?) : NetworkResponse<T>()
 
-
-
     val isSuccess: Boolean
         get() = this is Success && data != null
 }
